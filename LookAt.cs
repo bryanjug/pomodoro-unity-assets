a2dc22479996d84c1ffe7dd645e6c52e27c5e137
Bryan Jug <bryandjug@gmail.com>
2023-02-10 03:24:20 -0800

    added trees facing away from center of planet

--- LookAt.cs
+++ LookAt.cs
@@ -8,11 +8,21 @@ public class LookAt : MonoBehaviour
         if (transform.position.y > 0) {
             p[1] = 1f;
             transform.LookAt(p);
+
+            System.Random ran = new System.Random();
+            int x = ran.Next(-180, -210);
+            float xf = (float)x;
+
+            transform.Rotate(xf, 0.0f, 0.0f, Space.Self);
         }
         if (transform.position.y < 0) {
             transform.LookAt(Vector3.down);
-            //randomize x slightly
-            transform.Rotate(180.0f, 0.0f, 0.0f, Space.Self);
+            //randomize x slightly to make bottom trees more straight
+            System.Random ran = new System.Random();
+            int x = ran.Next(180, 210);
+            float xf = (float)x;
+
+            transform.Rotate(xf, 0.0f, 0.0f, Space.Self);
         }    
         Debug.Log("p: " + p);
         Debug.Log("Position: " + transform.position.y);
